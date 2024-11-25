5cf01a6f6156804908efee111ac2a8b7f6d22f33
Hentsuna <damtrongj095@gmail.com>
2024-11-25 22:14:53 +0700

    Update account.php

--- ChiaSeSach/account.php
+++ ChiaSeSach/account.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tài khoản</title>
+    <title>Thông tin cá nhân</title>
     <link rel="stylesheet" href="cssfolder/account.css">
     <link rel="stylesheet" href="cssfolder/header.css">
     <link rel="stylesheet" href="cssfolder/footer.css">
